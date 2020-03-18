191122fc59714968079bd89c98784c63c8805b6c
mlynarz21 <C4L4mit0u$>
2020-03-18 20:49:42 +0100

    added countries, locations, warehouses

--- startup/20_insert_regions.sql
+++ startup/20_insert_regions.sql
@@ -2,9 +2,14 @@ CONNECT usr/pwd@//localhost:1521/pdb;
 
 REM INSERTING into REGIONS
 SET DEFINE OFF;
-Insert into REGIONS (REGION_ID,REGION_NAME) values (1,'Europe');
-Insert into REGIONS (REGION_ID,REGION_NAME) values (2,'Americas');
-Insert into REGIONS (REGION_ID,REGION_NAME) values (3,'Asia');
-Insert into REGIONS (REGION_ID,REGION_NAME) values (4,'Middle East and Africa');
+INSERT ALL 
+	into REGIONS (REGION_ID,REGION_NAME) values (1,'Europe')
+	into REGIONS (REGION_ID,REGION_NAME) values (2,'North America')
+	into REGIONS (REGION_ID,REGION_NAME) values (3,'South America')
+	into REGIONS (REGION_ID,REGION_NAME) values (4,'Asia')
+	into REGIONS (REGION_ID,REGION_NAME) values (5,'Middle East')
+	into REGIONS (REGION_ID,REGION_NAME) values (6,'Africa')
+	into REGIONS (REGION_ID,REGION_NAME) values (7,'Oceania')
+SELECT * FROM DUAL;
 
 COMMIT;
