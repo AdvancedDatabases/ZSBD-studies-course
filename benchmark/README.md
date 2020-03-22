e959cb9f3590e1e4dc0769f9bf716ebebefb1e4a
mlynarz21 <C4L4mit0u$>
2020-03-22 14:57:06 +0100

    Revert "Merge branch 'master' of https://github.com/AdvancedDatabases/ZSBD-studies-course"
    This reverts commit 153110c88531fb377204169bd939bc664dd08a73, reversing
    changes made to 8946ebc7b0794bede617c435d24edf4ef23567ec.

--- benchmark/README.md
+++ benchmark/README.md
@@ -1,13 +1,13 @@
-# Testy wydajnościowe
+# testy wydajnościowe
 Katalog zawiera zestawy próbek obciążenia (skrypty SQL) na potrzeby przeprowadzenia
 testów wydajnościowych systemu zarządzania bazą danych Oracle 12c oraz skrypty
 uruchamiające te testy i mierzące czasy wykonania.
 
-# Lokalizacja próbek obciążenia
+## Lokalizacja próbek obciążenia
 Próbki obciążenia w postaci skryptów SQL znajdują się w katalogu **test-sets/NAME**,
 gdzie *NAME* to nazwa zbioru próbek obciążenia.
 
-# Architektura środowiska testowego
+## Architektura środowiska testowego
 Środowisko testowe składa się z:
 1. kontenera SZBD Oracle
 1. skryptów uruchamianych w kontenerze
@@ -23,37 +23,8 @@ gdzie *NAME* to nazwa zbioru próbek obciążenia.
     - są montowane w kontenerze (```/home/oracle/benchmark/test-sets```)
 
 # Uruchomienie testów wydajnościowych
-Dla każdego z zestawów można uruchomić testy podając nazwę zbioru testowego
-(nazwę katalogu z testami w głównym katalogu testów,  ```/home/oracle/benchmark/test-sets```)
-dla parametru *-i*, np:
 ```
 ./run.sh -i=set3
 ```
-
-## Odtwarzanie środowiska
-Jeżeli w zbiorze testowym występują skrpyty, które zmieniają stan bazy danych
-i wymagają przywrócenia bazy danych do stanu pierwotnego, to przed uruchomieniem
-skryptu *run.sh* należy w katalogu z testami utworzyć plik o nazwie *require_restart*
-i w tym pliku, w osobnych liniach wypisać nazwy (tzn. NAME.EXT bez ścieżek)
-plików testów, które wymagają odtwarzania środowiska.
-
-# Wyniki
-Wyniki testów wydajnościowych będą zapisane w pliku *benchmark_summary-SET_NAME.tsv*,
-gdzie *SET_NAME* to nazwa zbioru testowego. Plik zawiera podsumowanie czasów dla
-*każdego z testów*. W pliku zapisane są czasy: minimalny, średni, maksymalny oraz
-liczba uruchomień dla danego testu. Przykładowa zawartość pliku dla zbioru *dummy*:
-```
-test_name	n	min_time	avg_time	max_time
-test1.sql	3	0.047	0.062	0.089
-test2.sql	3	0.044	0.050	0.062
-test3.sql	3	0.042	0.052	0.065
-```
-
-# Logi
-W czasie testów zapisywane są logi z wykonania skrpytu run.sh (run.log) oraz
-worker.sh (worker.log). Ponadto, po uruchomieniu danego zbioru testowego w
-katalogu zbioru testowego zapisane są logi z czasami wykonania. Wszystkie logi
-są usuwane przed każdym kolejnym uruchomieniem skryptu run.sh.
-
-Wykorzystano modyfikację skryptu do logowania z następującego źródła:
-https://github.com/adoyle-h/bash-logger
+Parametr *-i* przyjmuje nazwę zbioru testowego (nazwę katalogu z testami w głównym
+katalogu testów, ```/home/oracle/benchmark/test-sets```)
--- benchmark/run.sh
+++ benchmark/run.sh
@@ -17,13 +17,9 @@ require_restart=''
 remote_workdir='/home/oracle/benchmark'
 remote_test_suites_dir_name='test-sets'
 worker_name='worker.sh'
-test_suite_cfg_name='require_restart'
+test_suite_cfg_name='config'
 remote_suite_files_list="$PWD/remote_scripts_list.tmp"
 container_name='zsbd-container'
-docker_compose_dir=$(as_abs '..')
-loaded_data_msg='DONE: Executing user defined scripts'
-benchmark_summary='benchmark_summary.tsv'
-
 
 # parse parameters
 for i in "$@"
@@ -56,51 +52,15 @@ case $i in
 esac
 done
 
-
-reset_container() {
-    INFO "Restarting container ..."
-    docker_compose_reset "$docker_compose_dir"
-    INFO "Waiting till container $container_name and database will be ready to use ..."
-    wait_for_container "$container_name" "$loaded_data_msg"
-    INFO "Container $container_name and database is ready to use!"
-}
-
-run_test() {
-    fail_if_empty "$1" && fail_if_empty "$2"
-    test_script_path="$1"
-    n_times=$2
-    cmd="$remote_worker -i=$test_script_path -n=$n_times"
-    container_exec "$container_name" "$cmd"
-}
-
-run_single_test_and_reset() {
+run_single_test() {
     fail_if_empty "$1"
-    INFO "Execute single test (no repetition) ..."
-    run_test "$test_path" 1
-    INFO "Restart container after test ..."
-    reset_container
+    test_script_path="$1"
+    cmd="$remote_worker -i=$test_script_path -n=$number_of_repeats"
+    container_exec "$container_name" "$cmd"
 }
 
 get_remote_test_paths() {
     get_remote_file_ext_paths "$remote_suite_dir" "$container_name" '*.sql' "$remote_suite_files_list"
-}
-
-need_restart_after_each_test() {
-    # for given test set, get names of tests which requres restart after Execution
-    fail_if_empty "$1" && fail_if_empty "$2"
-    test_path="$1"
-    test_suite_config="$2"
-    test_name=$( echo ${test_path##/*/} )
-    { grep "$test_name$" "$test_suite_config" &> /dev/null && return 0 ; } || return 1
-}
-
-gather_stats() {
-    fail_if_empty "$1" && fail_if_empty "$2" && fail_if_empty "$3"
-    measured_times_file="$1"
-    output_file="$2"
-    test_name="$3"
-    INFO "Calculating stats for $test_name ..."
-	"$PWD/calculate_stats" "$measured_times_file" "$output_file" "$test_name" --append
 }
 
 # import logging functions
@@ -118,43 +78,22 @@ if [[ -z "$remote_suite_name" ]]; then
 	exit 1
 fi
 
-INFO "Remove stale logs ..."
-remove_stale_logs "$PWD"
-rm -f "$benchmark_summary"
-
-# reset container
-reset_container
-
 # set required variable based on given parameters
-remote_test_suites_dir="$remote_workdir/$remote_test_suites_dir_name"  # /home/oracle/test-sets/dummy/require_restart
+remote_test_suites_dir="$remote_workdir/$remote_test_suites_dir_name"  # /home/oracle/test-sets
 remote_suite_dir="$remote_test_suites_dir/$remote_suite_name"
 fail_if_container_dir_not_exists "$remote_suite_dir" "$container_name"
 remote_worker="$remote_workdir/$worker_name"  # /home/oracle/worker.sh
-local_test_suite_config="$PWD/$remote_test_suites_dir_name/$remote_suite_name/$test_suite_cfg_name"
+test_suite_config="$remote_suite_dir/$test_suite_cfg_name"  # /home/oracle/
 get_remote_test_paths
 pass_if_nonempty_file "$remote_suite_files_list"
 
 tests_no=$(wc -l "$remote_suite_files_list")
-benchmark_summary="$PWD/benchmark_summary-$remote_test_suites_dir_name.tsv"
-INFO "Starting benchmark for $tests_no tests from test suite '$remote_test_suites_dir_name' ($remote_suite_dir) ..."
+INFO "Starting benchmark for $tests_no tests from $remote_suite_dir ..."
 while read test_path; do
-    test_name=$( echo ${test_path##/*/} )
-    need_restart_after_each_test "$test_path" "$local_test_suite_config"
-    if [ $? -eq 0 ]; then
-        INFO "Test '$test_name' requires reseting container after each execution ..."
-        for i in $(seq 1 $number_of_repeats); do
-            INFO "Starting $i / $number_of_repeats repetition of test $test_path ... "
-            run_single_test_and_reset "$test_path"
-        done
-    else
-        run_test "$test_path" $number_of_repeats
-    fi
-    INFO "All $number_of_repeats tests for 'test_name' executed."
-    # this assumes that this directory is mounted just like in docker-compose.yml
-    local_test_path="$PWD/$remote_test_suites_dir_name/$remote_suite_name/$test_name"
-    local_test_results_path="$local_test_path.log"
-    gather_stats "$local_test_results_path" "$benchmark_summary" "$test_name"
+    echo "$test_path"
+    run_single_test "$test_path"
+    # TODO read which scripts requre restart (config file)
+    # restart if need
+
 done <"$remote_suite_files_list"
-INFO "Test finished."
-INFO "Benchmark summary stored in $benchmark_summary"
 rm "$remote_suite_files_list"
