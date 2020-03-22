e30da473d570e79b53e236395d69e30921e9bef8
Palum12 <xoil123@gmail.com>
2020-03-22 21:15:27 +0100

    updated 1 sql set 2

--- benchmark/test-sets/set2/1.sql
+++ benchmark/test-sets/set2/1.sql
@@ -1,4 +1,5 @@
-select min(C.COUNTRY_NAME), count(LOCATION_ID)
-from COUNTRIES C join LOCATIONS L on C.COUNTRY_ID = L.COUNTRY_ID
-group by L.COUNTRY_ID
-having count(LOCATION_ID) > 1;
+-- select countries with at least ten warehouses
+select C.COUNTRY_NAME, count(W.WAREHOUSE_ID)
+from COUNTRIES C join LOCATIONS L on C.COUNTRY_ID = L.COUNTRY_ID join WAREHOUSES W on L.LOCATION_ID = W.LOCATION_ID
+group by C.COUNTRY_NAME
+having count(W.WAREHOUSE_ID) > 10;
