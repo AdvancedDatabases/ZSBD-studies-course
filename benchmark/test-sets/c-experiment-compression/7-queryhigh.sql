ce612fc1576136d241a706b55a6a66bbe66b2e36
Grzegorz Kostkowski <220891@student.pwr.edu.pl>
2020-06-07 13:47:04 +0200

    Adjust experiments, add missing slashes required in sqlplus

--- benchmark/test-sets/c-experiment-compression/7-queryhigh.sql
+++ benchmark/test-sets/c-experiment-compression/7-queryhigh.sql
@@ -1,5 +1,6 @@
 ALTER TABLE ORDER_ITEMS
 INMEMORY MEMCOMPRESS FOR QUERY HIGH;
+/
 
 SELECT table_name,
        inmemory,
@@ -9,3 +10,33 @@ SELECT table_name,
        inmemory_duplicate
 FROM   user_tables
 WHERE  table_name = 'ORDER_ITEMS';
+/
+
+UPDATE ORDER_ITEMS
+SET UNIT_PRICE =
+        (
+            SELECT MAX(ROUND(AVG(P.LIST_PRICE), 2))
+            FROM PRODUCTS P
+                     JOIN INVENTORIES I on P.PRODUCT_ID = I.PRODUCT_ID
+            GROUP BY I.WAREHOUSE_ID
+        )
+WHERE ORDER_ID IN
+      (
+          SELECT O.ORDER_ID
+          FROM (
+                   SELECT *
+                   FROM EMPLOYEES
+                   WHERE MANAGER_ID =
+                         (
+                             SELECT *
+                             FROM (
+                                      SELECT E.EMPLOYEE_ID
+                                      FROM EMPLOYEES E
+                                      WHERE E.HIRE_DATE >= TO_DATE('2016-01-01', 'yyyy-MM-dd')
+                                      ORDER BY E.HIRE_DATE
+                                  )
+                             WHERE ROWNUM <= 1
+                         )
+               ) EMP
+              JOIN ORDERS O ON O.SALESMAN_ID = EMP.EMPLOYEE_ID
+      );
--- benchmark/test-sets/c-experiment-no-column-compression/2-nomemcolumn.sql
+++ benchmark/test-sets/c-experiment-no-column-compression/2-nomemcolumn.sql
@@ -1,5 +1,6 @@
 ALTER TABLE ORDERS
 INMEMORY NO INMEMORY (STATUS);
+/
 
 SELECT table_name,
        inmemory,
@@ -9,3 +10,15 @@ SELECT table_name,
        inmemory_duplicate
 FROM   user_tables
 WHERE  table_name = 'ORDER_ITEMS';
+/
+
+select *
+from (
+    select min(m.FIRST_NAME) as first_name, min(m.LAST_NAME) as last_name, count(ORDER_ID) as count
+    from EMPLOYEES M join EMPLOYEES E On E.MANAGER_ID = M.EMPLOYEE_ID
+    join ORDERS O on E.EMPLOYEE_ID = O.SALESMAN_ID
+    where O.STATUS = 'Shipped'
+    group by e.MANAGER_ID
+    order by count(ORDER_ID) desc
+)
+where rownum <=5;
